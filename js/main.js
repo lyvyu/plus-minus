1c1f32ad7e79a8f4aef1adeb666cca7af464cacd
lyvyu <doloscan.liviu@gmail.com>
2015-12-03 17:57:23 +0200

    no message

--- js/main.js
+++ js/main.js
@@ -3,6 +3,46 @@ function randomInt(min,max)
     return Math.floor(Math.random()*(max-min+1)+min);
 }
 
+var i, data, Tiles, tiles;
+
+function setAttributes(el, attrs) {
+	for(var key in attrs) {
+		el.setAttribute(key, attrs[key]);
+	}
+}
+
+Tiles = function Tiles(data) {
+	var i,
+		div = document.createElement('div');
+
+	for(i in data) {
+		var dataId =  'data-'+ i;
+		setAttributes(div, {dataId: data[i]});
+	}
+
+	div.innerHTML = data.album_name;
+
+	for(i in this) {
+		if(typeof this[i] === 'function') {
+			div[i] = this[i].bind(div);
+		} else {
+			div[i] = this[i];
+		}
+	}
+
+	return div;
+};
+
+data = [
+	{'id':1,'album_name':'test1','user':'user_id1'},
+	{'id':2,'album_name':'test2','user':'user_id2'}
+];
+
+for(i in data) {
+	tiles = new Tiles(data[i]);
+	document.body.appendChild(tiles);
+}
+
 function game() {	
 	$('.number-swatch').each(function() {
 		var rand = randomInt(-9,9);
