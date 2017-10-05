87335a5e79000984ed2b0160fab2a75649352aea
ldoloscan <liviu_17>
2017-10-05 18:05:11 +0300

    - started writing bot

--- js/main.v2.js
+++ js/main.v2.js
@@ -1,86 +1,89 @@
-function randomInt(min, max)
-{	
-    let rand = Math.floor(Math.random() * (max - min + 1) + min);
+function randomInt(min, max) {
+  let rand = Math.floor(Math.random() * (max - min + 1) + min);
 
-	return rand;
+  return rand;
 }
 
 function setAttributes(el, attrs) {
-	for(var key in attrs) {
-		el.attr(key, attrs[key]);
-	}
+  for (var key in attrs) {
+    el.attr(key, attrs[key]);
+  }
 }
 
 function Tile(number, row, col) {
-	this.div = $('<div/>', { class: 'col-xs-2 number-swatch bg-primary' });
-	let span = $('<span>', { class: "number-container", text: number });
+  this.div = $('<div/>', {class: 'col-xs-2 number-swatch bg-primary'});
+  let span = $('<span>', {class: "number-container", text: number});
 
-	this.div.attr({
-		'data-id': 'id_' + row + col,
-		'data-row': row,
-		'data-col': col,
-		'data-context': number
-	});
+  this.div.attr({
+    'data-id': 'id_' + row + col,
+    'data-row': row,
+    'data-col': col,
+    'data-context': number
+  });
 
-	span.appendTo(this.div);
+  span.appendTo(this.div);
 
-	return this.div;
+  return this.div;
 }
 
 function Game() {
-	this.playingArea = $('#playing-area');
-	this.score = {
-		me: $('.score.me > span'),
-		them: $('.score.them > span')
-	};
+  this.playingArea = $('#playing-area');
+  this._busy = false;
 
-	this.render = () => {
-		this.playingArea.html('');
-		this.score['me'].text(0);
-		this.score['them'].text(0);
+  this.score = {
+    me: $('.score.me > span'),
+    them: $('.score.them > span')
+  };
 
-		for (let i = 1; i <= 5; i++) {
-			for (let j = 1; j <= 5; j++) {
-				let randNr = randomInt(-9, 9);
+  this.render = () => {
+    this.playingArea.html('');
+    this.score['me'].text(0);
+    this.score['them'].text(0);
 
-				this.playingArea.append(new Tile(randNr, i, j));
-			}
-		}
+    for (let i = 1; i <= 5; i++) {
+      for (let j = 1; j <= 5; j++) {
+        let randNr = randomInt(-9, 9);
 
-		let activeRow = randomInt(1, 5);
-		this.activateRow(activeRow);
-	}
+        this.playingArea.append(new Tile(randNr, i, j));
+      }
+    }
 
-	this.activateRow = (row) => {
-		$('.number-swatch').removeClass('bg-blue bg-pink mine theirs');
-		$('.number-swatch[data-row="'+ row +'"]').each((idx, elm) => {
-			setTimeout(() => {
-				$(elm).addClass('bg-blue mine');
-			}, idx * 110)
-		});
-	}
+    let activeRow = randomInt(1, 5);
+    this.activateRow(activeRow, () => {
+      console.log('done')
+    });
+  }
 
-	this.activatecol = (col) => {
-		$('.number-swatch').removeClass('bg-blue bg-pink mine theirs');
-		$('.number-swatch[data-col="'+ col +'"]').each((idx, elm) => {
-			setTimeout(() => {
-				$(elm).addClass('bg-pink theirs');
-			}, idx * 110)
-		});
-	}
+  this.activateRow = (row) => {
+    $('.number-swatch').removeClass('bg-blue bg-pink mine theirs');
+    $('.number-swatch[data-row="' + row + '"]').each((idx, elm) => {
+      setTimeout(() => {
+        $(elm).addClass('bg-blue mine');
+      }, idx * 110)
+    });
+  };
 
-	this.updateScore = (which, score) => {
-		let current = this.score[which].text();
-		this.score[which].text(+current + score);
-	}
+  this.activatecol = (col) => {
+    $('.number-swatch').removeClass('bg-blue bg-pink mine theirs');
+    $('.number-swatch[data-col="' + col + '"]').each((idx, elm) => {
+      setTimeout(() => {
+        $(elm).addClass('bg-pink theirs');
+      }, idx * 110)
+    });
+  }
 
-	this.run = () => {
-		this.render();
-	}
+  this.updateScore = (which, score) => {
+    let current = this.score[which].text();
+    this.score[which].text(+current + score);
+  }
 
-	this.reset = () => {
-		this.run();
-	}
+  this.run = () => {
+    this.render();
+  }
+
+  this.reset = () => {
+    this.run();
+  }
 }
 
 
@@ -88,81 +91,130 @@ const game = new Game();
 
 game.run();
 
-$(document).on('click', '.refresh-btn', (e) => {	
-	e.preventDefault();
+$(document).on('click', '.refresh-btn', (e) => {
+  e.preventDefault();
 
-	game.reset();
+  game.reset();
 });
 
 $(document).on('click', '.number-swatch.mine', function (elm) {
-	let $this = $(this);
-	let clone = $this.clone();
-	let offset = $this.position();
-	let col = $this.data('col');
-	let points = $this.data('context');
+  let $this = $(this);
+  let clone = $this.clone();
+  let offset = $this.position();
+  let col = $this.data('col');
+  let points = $this.data('context');
 
-	$this.addClass('hidden-number');
+  $this.addClass('hidden-number');
 
-	game.updateScore('me', points);
+  game.updateScore('me', points);
 
-	clone
-	.css({
-			'position':'absolute',
-			'top': offset.top,
-			'left': offset.left
-		})
-	.addClass('cloned').appendTo($this.parent())	
-	.animate({'top': -100, 'opacity': 0})
-	.fadeOut(function () {
-		clone.remove();
+  clone
+     .css({
+       'position': 'absolute',
+       'top': offset.top,
+       'left': offset.left
+     })
+     .addClass('cloned').appendTo($this.parent())
+     .animate({'top': -100, 'opacity': 0})
+     .fadeOut(function () {
+       clone.remove();
 
-		game.activatecol(col);
+       game.activatecol(col);
 
-		setTimeout(() => {
-			_botPlayer(col);
-		}, 1000)
-	});	
+       setTimeout(() => {
+         _botPlayer(col);
+       }, 1000)
+     });
 });
 
+/**
+ * BOT player
+ * */
 function _botPlayer(col) {
-	let selectedTile = $('.number-swatch[data-col='+ col +']')
-		.not('.hidden-number').get()
-		.map((elm) => {
-			return {
-				nr: $(elm).data('context'),
-				element: elm
-			};
-		})
-		.reduce((prev, current) => {
-			return (prev.nr > current.nr) ? $(prev.element) : $(current.element);
-		});
+  let arr = [];
+  let grid = $('.number-swatch')
+     .not('.hidden-number').get()
+     .forEach((elm) => {
+       let row = $(elm).data('row');
 
-	let clone = selectedTile.clone();
-	let offset = selectedTile.position();
-	let row = selectedTile.data('row');
-	let points = selectedTile.data('context');
+       if (!arr[row]) {
+         arr[row] = [];
+       }
 
-	selectedTile.addClass('hidden-number');
+       arr[row].push({
+         nr: $(elm).data('context'),
+         element: elm
+       })
 
-	game.updateScore('them', points);
+     });
 
-	clone
-	.css({
-			'position':'absolute',
-			'top': offset.top,
-			'left': offset.left
-		})
-	.addClass('cloned').appendTo(selectedTile.parent())	
-	.animate({'top': -100, 'opacity': 0})
-	.fadeOut(function () {
-		clone.remove();
+  let c = {};
+  let min = arr.map((row, idx) => {
+    let minVal = Math.min.apply(Math, row.map(function(o){return o.nr;}));
+    let count = 0;
 
-		game.activateRow(row);
+    return row.filter(item => {
+      let elmCol = $(item.element).data('col');
 
-		// setTimeout(() => {
-			// _botPlayer(col);
-		// }, 1000)
-	});	
+      if (item.nr < 0) {
+        count++;
+      }
+
+      c[idx] = count;
+
+      return elmCol === col && item.nr > minVal;
+    });
+  }).filter(item => {
+    return item.length > 0;
+  })[0];
+
+  // let minn =
+
+  console.log(c)
+  return;
+
+  let selectedTile = $(min[0].element);
+     // $('.number-swatch[data-col=' + col + ']')
+     // .not('.hidden-number').get()
+     // .map((elm) => {
+     //   return {
+     //     nr: $(elm).data('context'),
+     //     element: elm
+     //   };
+     // })
+     // .reduce((prev, current) => {
+     //   return (prev.nr > current.nr) ? $(prev.element) : $(current.element);
+     // });
+
+  let clone = selectedTile.clone();
+  let offset = selectedTile.position();
+  let row = selectedTile.data('row');
+  let points = selectedTile.data('context');
+
+  // console.log(selectedTile)
+  // return;
+  // debugger;
+  selectedTile.addClass('hidden-number');
+
+  game.updateScore('them', points);
+
+  clone
+     .css({
+       'position': 'absolute',
+       'top': offset.top,
+       'left': offset.left
+     })
+     .addClass('cloned').appendTo(selectedTile.parent())
+     .animate({'top': -100, 'opacity': 0})
+     .fadeOut(function () {
+       clone.remove();
+
+       game.activateRow(row);
+     });
+}
+
+function CalcBestMove() {
+
 }
 
 
